0884f8f56be2216d2582d0838db6250fce642a2b
Alejandro Pinel Martínez <55890199+alekpinel@users.noreply.github.com>
2021-12-16 23:25:46 +0100

    Challenge 10

--- Challenge 10/Challenge10.py
+++ Challenge 10/Challenge10.py
@@ -26,25 +26,25 @@ def calculateChecksum(buffer):
 def processCase(case):
     # print (case)
     
-    integers = []
-    for hex in case:
-        integers.append(int(hex, 16))
+    # integers = []
+    # for hex in case:
+    #     integers.append(int(hex, 16))
     
-    # print (len(integers))
+    # # print (len(integers))
     
-    chars = []
-    for integer in integers:
-        chars.append(chr(integer))
+    # chars = []
+    # for integer in integers:
+    #     chars.append(chr(integer))
     
-    chars = []
-    for i in range(0, len(integers), 3):
-        print(integers[i] + ord('R'))
-        print(integers[i+1] + ord('F'))
-        print(integers[i+2] + ord('C'))
-        intValue = (integers[i] + integers[i+1] + integers[i+2])%255
-        chars.append(chr(intValue))
+    # chars = []
+    # for i in range(0, len(integers), 3):
+    #     print(integers[i] + ord('R'))
+    #     print(integers[i+1] + ord('F'))
+    #     print(integers[i+2] + ord('C'))
+    #     intValue = (integers[i] + integers[i+1] + integers[i+2])%255
+    #     chars.append(chr(intValue))
     
-    print(chars)
+    # print(chars)
     
     # megaString = ''
     # for i in range(192):
@@ -64,29 +64,107 @@ def processCase(case):
     # entire_chk = calculateChecksum(entire_message)
     # print (entire_chk, '{:x}'.format(entire_chk), '(host byte order)')
     
-    return None
+    case.sort(key=lambda x:x['checksum'])
+    
+    case.sort(key=lambda x: (x['time']))
+    # case.reverse()
+    
+    reverseWord = ['' for i in range(213)]
+    word = ''
+    for message, i in zip(case, range(len(case))):
+        word += chr((int(message['id'], 16)))
+        
+        # word += chr((int(message['id'], 16) + int(message['data'], 16))%255)
+        # word += message['otherId']
+        
+        # word += chr((int(case[(message['seq'])%213]['id'], 16)))
+        # reverseWord[(message['seq'])%213] = chr((int(message['id'], 16)))
+        
+        print(message)
+    
+    # print(''.join(reverseWord))
+    return word
 
 
-def readCase(file):
-    data = []
-    for i in range(213):
+def readDataPacket(messages):
+    file = open("packet.txt", "r", encoding="utf-8")
+
+    for message in messages:
         line = file.readline()
         while ('Data (1 byte)' not in line):
             line = file.readline()
         line = file.readline()
         line = file.readline()
-        data.append(line[6:8])
-    return data
+        message['data'] = line[6:8]
+        # data.append(line[6:8])
+    file.close()
+    
+def readDataPacket(messages):
+    file = open("packet.txt", "r", encoding="utf-8")
+
+    for message in messages:
+        line = file.readline()
+        while ('Data (1 byte)' not in line):
+            line = file.readline()
+        line = file.readline()
+        line = file.readline()
+        message['data'] = '0x' + line[6:8]
+        # data.append(line[6:8])
+    file.close()
+    
+def readSeqAndIdPacket(messages):
+    file = open("packet.csv", "r", encoding="utf-8")
+    line = file.readline()
+    for message, i in zip(messages, range(len(messages))):
+        line = file.readline().replace('\"', '')
+        info = line.split(',')
+        extraInfo = info[6].split(' ')
+        
+        message['time'] = float(info[1])
+        message['id'] = '0x' + info[6].split('=')[1][4:6]
+        message['seq'] = int(info[7].split('=')[1].split('/')[0])
+        message['seq2'] = int(info[7].split('/')[1])
+        
+        # message['seq'] = i
+        
+    for message, i in zip(messages, range(len(messages))):
+        
+        msg_type = '\x08' # ICMP Echo Request
+        msg_code = '\x00' # must be zero
+        msg_checksum_padding = '\x00\x00' # "...with value 0 substituted for this field..."
+        
+        id_bytes = chr(int(message['id'], 16))
+        seq_bytes = chr(message['seq'])
+        
+        payload_body = chr(int(message['data'], 16))
+        
+        # id_bytes = chr(int(messages[message['seq']%213]['id'], 16))
+        # message['otherId'] = chr(int(messages[message['seq']%213]['id'], 16))
+        # payload_body = chr(int(messages[message['seq']%213]['data'], 16))
+        
+        rest_header = '\x00' + id_bytes + '\x00' + seq_bytes # from pcap
+        
+        entire_message = msg_type + msg_code + msg_checksum_padding + rest_header + payload_body
+        # message['header'] = entire_message
+        entire_chk = calculateChecksum(entire_message)
+        # message['checksum'] = '{:x}'.format(entire_chk)
+        message['checksum'] = hex(entire_chk)
+        message['checksum'] = entire_chk
+    file.close()
+
 
 #Get input
-def readInput(inputfile, caseProcessor):
-    f = open(inputfile + ".txt", "r", encoding="utf-8")
-    nlines = 1
-    lines = []
-    for i in range(nlines):
-        lines.append(caseProcessor(f))
-    f.close()
-    return lines
+def readInput(inputfile):
+    
+    
+    nMessages = 213
+    messages = [{} for i in range(nMessages)]
+    
+    readDataPacket(messages)
+    readSeqAndIdPacket(messages)
+    
+    
+    return messages
 
 #Write the output
 class OutputWriter:
@@ -115,13 +193,12 @@ def main():
     if (not writeFile):
         outputfile = None
 
-    inputs = readInput(inputfile, readCase)
+    inputs = readInput(inputfile)
 
 
     outputWriter = OutputWriter(outputfile)
-    for input in inputs:
-        output = processCase(input)
-        outputWriter(output)
+    output = processCase(inputs)
+    outputWriter(output)
 
 if __name__ == "__main__":
     main()
